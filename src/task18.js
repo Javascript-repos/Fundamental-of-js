c5c2b30ca2b6c36cf33b53343d80bc39b8d0fc77
Sujata Mishra <mishrasujata1628@gmail.com>
2020-01-07 18:22:41 +0000

    Update task18.js

--- src/task18.js
+++ src/task18.js
@@ -15,3 +15,8 @@
 * test : npm run test18
 * exec : npm run task18
 **/
+module.exports = getEight = function(number1,number2){
+    if(number1===8 || number2===8 || number1+number2===8)
+        return true;
+    return false;
+}
